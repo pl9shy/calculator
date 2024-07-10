7b77e02159b433148f1966fb38ef9f97104c9c0c
pl9shy <neo.wwh1@gmail.com>
2024-07-10 12:43:30 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите первое число: ");
+string firstNum = Console.ReadLine();
+
+Console.Write("Введите второе число: ");
+string secondNum = Console.ReadLine();
+
+Console.Write($"Какую операцию выполнить?\n+\t-\t*\t/");
+string Symbol = Console.ReadLine();
+
+if (Symbol == "+")
+{
+    int result = int.Parse(firstNum) + int.Parse(secondNum);
+    Console.WriteLine($"{firstNum} + {secondNum} = {result}");
+}
+if (Symbol == "-")
+{
+    int result = int.Parse(firstNum) - int.Parse(secondNum);
+    Console.WriteLine($"{firstNum} - {secondNum} = {result}");
+}
+if (Symbol == "*")
+{
+    int result = int.Parse(firstNum) * int.Parse(secondNum);
+    Console.WriteLine($"{firstNum} * {secondNum} = {result}");
+}
+if (Symbol == "/")
+{
+    int result = int.Parse(firstNum) / int.Parse(secondNum);
+    Console.WriteLine($"{firstNum} : {secondNum} = {result}");
+}
+Console.ReadLine();
